6948b378be15fbebf2962eafaed248b240bb84bd
Vadym <vadymlysytskyi@gmail.com>
2024-04-10 13:02:37 +0200

    10

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,16 +3,18 @@ import css from "../ImageGallery/ImageGallery.module.css";
 const ImageGallery = ({ items, onClick }) => {
   return (
     <ul className={css.imageGallery}>
-      {items.map((item) => {
-        return (
-          <ImageCard
-            key={item.id}
-            imageUrl={item.small}
-            alt={item.alt}
-            onClick={() => onClick(item)}
-          />
-        );
-      })}
+      {items.length > 0 &&
+        items.map((item) => {
+          return (
+            <li key={item.id}>
+              <ImageCard
+                imageUrl={item.small}
+                alt={item.alt}
+                onClick={() => onClick(item)}
+              />
+            </li>
+          );
+        })}
     </ul>
   );
 };
